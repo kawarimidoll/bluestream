49514cb4c6078ee031d379d36b4ed14153d8b483
kawarimidoll <kawarimidoll+git@gmail.com>
2023-06-20 19:42:49 +0900

    chore(deps): update dependencies

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
 import {
   sanitize,
   tagNoVoid as tag,
@@ -7,7 +7,7 @@ import {
 import BskyAgent, {
   AppBskyActorDefs,
   AppBskyFeedDefs,
-} from "https://esm.sh/@atproto/api@0.2.10";
+} from "https://esm.sh/@atproto/api@0.3.12";
 type ProfileViewDetailed = AppBskyActorDefs.ProfileViewDetailed;
 type FeedViewPost = AppBskyFeedDefs.FeedViewPost;
 type PostView = AppBskyFeedDefs.PostView;
